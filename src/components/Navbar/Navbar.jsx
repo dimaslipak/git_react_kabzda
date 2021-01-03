bc03b87950b7d2713681a8e303d43fd4f6076810
Dzmitry Dz <dimaslipak@gmail.com>
2021-01-03 22:27:24 +0100

    react 20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='/Profile'>Profile </a>
+                <NavLink to='/Profile' activeClassName={s.active}>Profile </NavLink>
             </div>
             <div className={s.item}>
-                <a href='/Dialogs'>Messages</a>
-            </div>
-            <div className={`${s.item} ${s.active}`}>
-                News
+                <NavLink to='/Dialogs' activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                Music
+                <NavLink to='/News' activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
-                Settings
+                <NavLink to='/Music' activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/Settings' activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
     )
